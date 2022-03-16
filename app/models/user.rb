d4c35d1073e8b9f990b53daf63acde5f79e37626
Edwin Ly <edwinjly@gmail.com>
2022-03-15 20:06:47 -0400

    Add Home page feature Spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, confirmation:true, length: {minimum: 6}
+  validates :password_confirmation, presence: true
+
+  def create
+  if user = User.authenticate_with_credentials(params[:email], params[:password])
+    # success logic, log them in
+  else
+    # failure, render login form
+  end
+end
 end
